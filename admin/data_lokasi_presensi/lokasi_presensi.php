9d625f0447575b9efecd0cbf229dae517f9ec2b3
EricSiahaan <eric.hansdeka78@gmail.com>
2024-02-06 16:51:59 +0700

    feet: add latitude, longitude

--- admin/data_lokasi_presensi/lokasi_presensi.php
+++ admin/data_lokasi_presensi/lokasi_presensi.php
@@ -43,14 +43,15 @@ $result = mysqli_query($connection, "SELECT * FROM lokasi_presensi ORDER BY id D
                         <td><?= $lokasi['latitude'] . '/' . $lokasi['longitude'] ?></td>
                         <td><?= $lokasi['radius'] ?></td>
                         <td class="text-center">
-                            <a href="<?= base_url('admin/data_lokasi_presensi/detail.php/' . $lokasi['id']) ?>" class="badge badge-pill bg-success">Detail</a>
-                            <a href="<?= base_url('admin/data_lokasi_presensi/edit.php/' . $lokasi['id']) ?>" class="badge badge-pill bg-primary">Edit</a>
-                            <a href="<?= base_url('admin/data_lokasi_presensi/hapus.php/' . $lokasi['id']) ?>" class="badge badge-pill bg-danger">Hapus</a>
+                            <a href="<?= base_url('admin/data_lokasi_presensi/detail.php?id=' . $lokasi['id']) ?>" class="badge badge-pill bg-success">Detail</a>
+                            <a href="<?= base_url('admin/data_lokasi_presensi/edit.php?id=' . $lokasi['id']) ?>" class="badge badge-pill bg-primary">Edit</a>
+                            <a href="<?= base_url('admin/data_lokasi_presensi/hapus.php?id=' . $lokasi['id']) ?>" class="badge badge-pill bg-danger tombol-hapus">Hapus</a>
                         </td>
                     </tr>
                 <?php endwhile; ?>
-
             <?php }  ?>
         </table>
     </div>
 </div>
+
+<?php include('../layout/footer.php');
